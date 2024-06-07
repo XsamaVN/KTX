d4d3b1523e69b3c4227ddbebb7f82def9c1adb45
NgocLong155 <ngoclong15596@gmail.com>
2024-06-07 13:22:47 +0700

    push

--- src/main/java/com/example/jwtspring3/model/Device.java
+++ src/main/java/com/example/jwtspring3/model/Device.java
@@ -1,8 +1,10 @@
 package com.example.jwtspring3.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class Device {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -10,37 +12,12 @@ public class Device {
     private String name;
     @Column(length = 1000)
     private String img;
-
+    private int quantity;
     public Device(Long id, String name, String img) {
         this.id = id;
         this.name = name;
         this.img = img;
     }
-
     public Device() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
     }
 }
